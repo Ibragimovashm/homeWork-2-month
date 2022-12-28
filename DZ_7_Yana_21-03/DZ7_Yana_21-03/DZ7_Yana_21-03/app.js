3c05fdf76d0fcad98cc2635d33bc3086b53ac459
Ibragimovashm <119935183+Ibragimovashm@users.noreply.github.com>
2022-12-28 13:34:13 +0600

    Add files via upload

---
+++ DZ_7_Yana_21-03/DZ7_Yana_21-03/DZ7_Yana_21-03/app.js
@@ -0,0 +1,30 @@
+/////////////////////////////add task/////////////////////
+document.querySelector(".addBtn").onclick = function () {
+  if (document.querySelector("input").value === "") {
+    alert("введите текст");
+  } else {
+    document.querySelector(".tasks").innerHTML += `<div class="task">
+          <span  class="taskName">${
+            document.querySelector("input").value
+          }</span>
+          <button class="delete">x</button>
+      </div>`;
+    const clearTask = document.querySelector("input");
+    clearTask.value = "";
+
+    //////////////delete task////////////
+    const current_tasks = document.querySelectorAll(".delete");
+    for (let i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+    ///////done task////////
+    let doneTask = document.querySelectorAll(".taskName");
+    for (let i = 0; i < doneTask.length; i++) {
+      doneTask[i].onclick = function () {
+        this.classList.toggle("completed");
+      };
+    }
+  }
+};
